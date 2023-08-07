3d294aa41926a84955943bca16190cca16a62ee4
d-ngnr <realngnr@gmail.com>
2023-08-06 22:58:27 -0500

    Create two-sums_brute_force.py

---
+++ two-sums/python/two-sums_brute_force.py
@@ -0,0 +1,24 @@
+class BruteForce(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums) # get the length of the list
+        for i in range(n - 1): # iterate the elements in the array starting from index 0 to the 2nd to the last element
+            for j in range(i + 1, n): # iterate the elements from the next element to the last element
+                if nums[i] + nums[j] == target: # add the numbers and check if it equals the target
+                    return [i,j] # return the indices if the sum of numbers matches the target
+        return [] # return nothig if the nothing satisfies the condition
+    
+def main():
+    bf = BruteForce() # create an instance of BruteForce
+    nums = [2,7,11,15] # declare the list of numbers
+    target = 9 # declare the target sum of numbers
+    print(bf.twoSum(nums, target)) # print the function return
+    
+if __name__ == '__main__':
+    main()
+    
+
