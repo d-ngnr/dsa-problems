0cb159cb558e334dad8bbe96b4b10ccc9188defc
d-ngnr <realngnr@gmail.com>
2023-08-10 21:51:56 -0500

    Update brute_force.py

--- best-time-to-buy-stocks/python/brute_force.py
+++ best-time-to-buy-stocks/python/brute_force.py
@@ -1,25 +1,22 @@
 from typing import List
 class BruteForceApproach(object):
-    def isValid(self, string: str) -> bool:
-        stack = [] # create an empty list to store the opening brackers
-        for char in string: # loop through the characters in the string
-            if char in '([{': # check if the character is an open bracket
-                stack.append(char) # if it is an open bracket then push it onto the stack
-            else: # if a character is a closing bracket
-                if not stack or \
-                    (char == ')' and stack[-1] != '(') or \
-                    (char == '}' and stack[-1] != '{') or \
-                    (char == ']' and stack[-1] != '['):
-                    return False # the string is not valid at this point so return False
-                stack.pop() # else, remove the opening bracket from the stack
-        return not stack # if the stack is empty then all of the opening brackets have been
-                         # matched with a matching closing bracket (string is valid) so return True
-                         # otherwise, return False
-
+    def maximumProfit(self, prices: List) -> int:
+        left = 0
+        right = 1
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left]
+            if prices[left] < prices[right]:
+                max_profit = max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
+        
 def main():
     bf = BruteForceApproach() # create an instance of BruteForce
-    string = "()[]{}" # declare string
-    print(bf.isValid(string)) # print the function return
+    prices = [7,1,5,3,6,4] # declare the list of prices
+    print(bf.maximumProfit(prices)) # print the function return
     
 if __name__ == '__main__':
     main()
